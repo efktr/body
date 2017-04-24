24de98123b4a6f816a78bf123e0753f5e8610f69
Christian Dallago <code@dallago.us>
2017-04-24 16:20:28 +0200

    added leaflet: working on creating coordinate system

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 import React, {Component} from 'react'
 import image from '../resources/body.png'
+import { Map, ImageOverlay} from 'react-leaflet'
+import './style.css'
+
+let Leaflet = require('leaflet');
+
 
 export default class EfktrBody extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            map: <div></div>
+        };
+
+        let img = new Image();
+        img.src = image;
+
+        let self = this;
+
+        img.onload = function(){
+            let width = this.width;
+            let height = this.height;
+
+            while (width > 500 || height > 500) {
+                width /= 2;
+                height /= 2;
+            }
+
+            let imageBounds = [[0, width], [height, 0]];
+
+            self.setState({
+                map: (
+                    <Map
+                        className="EfktrBodyMap"
+                        center={[height / 2, width / 2]}
+                        maxBounds={[[-100, width + 100], [height + 100, -100]]}
+
+            >
+                        <ImageOverlay
+                            url={img.src}
+                            bounds={imageBounds}
+                        />
+                    </Map>
+                )
+            });
+        };
+    }
+
     render() {
-        return <div>
-            <img src={image}/>
-        </div>
+        return this.state.map
     }
 }
