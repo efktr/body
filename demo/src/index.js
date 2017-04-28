20ca5c81f28b2cc9e018a361b4c8041706f4a5c2
Christian Dallago <code@dallago.us>
2017-04-28 14:08:49 +0200

    Got stub working

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,12 +4,14 @@ import {render} from 'react-dom'
 import EfktrBody from '../../src'
 
 let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>efktr-body-react Demo</h1>
-      <EfktrBody/>
-    </div>
-  }
+    render() {
+        return <div>
+          <h1>Demo</h1>
+          <EfktrBody onBodyPart={function(bodyParts){
+              console.log(bodyParts)}
+          }/>
+        </div>
+    }
 });
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,72 +1,95 @@
-import React, {Component} from 'react'
-import image from '../resources/body.png'
-import { Map, ImageOverlay} from 'react-leaflet'
-import './style.css'
+import React, {Component} from 'react';
+import image from '../resources/body.png';
+import { Map, ImageOverlay, Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../css/style.css';
+import { CRS } from 'leaflet';
+import '../css/signal.css';
+import { divIcon } from 'leaflet';
 
-import Leaflet from 'leaflet/dist/leaflet';
-//import LeafletCss from 'leaflet/dist/leaflet.css';
+const imageSize = {
+    width: 640,
+    height: 2201
+};
 
 export default class EfktrBody extends Component {
-    constructor(){
-        super();
+
+    constructor(props){
+        super(props);
+
+        this.passElements = props.onBodyPart || function(){};
+
+        let width = imageSize.width;
+        let height = imageSize.height;
+
+        while (width > 400 || height > 400) {
+            width /= 2;
+            height /= 2;
+        }
+
+        let imageBounds = [[0, width], [height, 0]];
 
         this.state = {
-            map: <div></div>
+            map: {
+                width: width,
+                height: height,
+                bounds: imageBounds
+            },
+            signal: undefined
         };
 
-        let img = new Image();
-        img.src = image;
-
-        let self = this;
-
-        img.onload = function(){
-            let width = this.width;
-            let height = this.height;
-
-            while (width > 500 || height > 500) {
-                width /= 2;
-                height /= 2;
-            }
-
-            let imageBounds = [[0, width], [height, 0]];
-
-            self.setState({
-                map: (
-                    <Map
-                        className="EfktrBodyMap"
-                        zoom={1}
-                        maxZoom={2}
-                        minZoom={0}
-                        center={[height, width]}
-                        maxBounds={[[0, width], [height, 0]]}
-                        crs={Leaflet.CRS.Simple}
-                        scrollWheelZoom={false}
-                        tap={false}
-                        touchZoom={false}
-                        keyboard={false}
-                        zoomAnimation={false}
-                        dragging={false}
-                        doubleClickZoom={false}
-                        boxZoom={false}
-                        onClick={this.pointClicked}
-            >
-                        <ImageOverlay
-                            url={image}
-                            bounds={imageBounds}
-                        />
-                    </Map>
-                )
-            });
-        };
+        this.pointClicked = this.pointClicked.bind(this);
     }
 
     pointClicked(event) {
-        console.log("test");
+        this.setState({
+            signal: event.latlng
+        });
+
+        this.passElements(['head', 'arm']);
     }
 
     render() {
-        return <div>
-            {this.state.map}
-        </div>
+        const marker = this.state.signal ? <Marker
+            icon={
+                divIcon({
+                        iconSize: [30, 30],
+                        iconAnchor: [10, 10],
+                        popupAnchor: [10, 0],
+                        shadowSize: [0, 0],
+                        className: 'animated-icon'
+                    }
+                )
+            }
+            position={this.state.signal}
+        /> : null;
+
+        return (
+            <Map
+                className="EfktrBodyMap"
+                zoom={1}
+                maxZoom={1}
+                minZoom={1}
+                center={[this.state.map.height, this.state.map.width]}
+                maxBounds={[[0, this.state.map.width], [this.state.map.height, 0]]}
+                crs={CRS.Simple}
+                scrollWheelZoom={false}
+                tap={false}
+                touchZoom={false}
+                keyboard={false}
+                zoomAnimation={false}
+                dragging={false}
+                doubleClickZoom={false}
+                boxZoom={false}
+                onClick={this.pointClicked}
+                ref="map"
+            >
+                <ImageOverlay
+                    url={image}
+                    bounds={this.state.map.bounds}
+                />
+                {marker}
+            </Map>
+        )
     }
 }
